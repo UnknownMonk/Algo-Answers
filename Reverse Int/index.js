894663c66bc70d10bf11aae49fe11ede15861256
UnknownMonk <smodcartoon@gmail.com>
2018-05-11 08:36:50 -0400

    reversing a intager negitive or positive number

---
+++ Reverse Int/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+ const number = n.toString().split('').reverse().join("");
+ if (n < 0) {
+   return parseInt(number) * -1;
+ }
+ return parseInt(reversed);
+}
+
+module.exports = reverseInt;
+
+
+// function reverseInt(n) {
+//   const number = n
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .join("");
+  
+//     return parseInt(number) * Math.sign(n);
+  
+// }
