2b145ef3977fb5321f740d83cc1e2380fe2461f8
UnknownMonk <smodcartoon@gmail.com>
2018-05-13 10:17:07 -0400

    Chunk a array into a smaller array

---
+++ Chunk/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  const chunked =[];
+  for(let el of array){
+    const last = chunked[chunked.length -1];
+    if(!last || last.length === size) {
+      chunked.push([el]);
+    }else{
+      last.push(el);
+    }
+  }
+  return chunked;
+}
+
+module.exports = chunk;
